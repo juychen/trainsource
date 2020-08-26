f17b5db749aa032fb62471212b04ef5281aca9d2
WINGHARE <WINGHARE@GMAIL.COM>
2020-08-26 22:09:45 +0800

    initial

---
+++ models.py
@@ -0,0 +1,64 @@
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from torch.autograd import Variable
+#import scipy.io as sio
+import numpy as np
+import pandas as pd
+
+
+# Model of AE
+class AE(nn.Module):
+    def __init__(self,dim_au_in,dim_au_out):
+        super(AE, self).__init__()
+        self.dim = dim_au_in
+        self.fc1 = nn.Linear(dim_au_in, 1024)
+
+        self.fc2 = nn.Linear(1024, dim_au_out)
+        self.fc3 = nn.Linear(dim_au_out, 1024)
+
+        self.fc4 = nn.Linear(1024, dim_au_in)            
+                    
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc2(h1)
+       
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        z = self.encode(x.view(-1, self.dim))
+        #return self.decode(z), x
+        return self.decode(z), z
+    
+    def fit(self, x):
+        return 
+
+
+class DNN(nn.Module):
+    def __init__(self,dim_dnn_in,dim_dnn_out):
+        super(DNN, self).__init__()
+        self.dim = dim_dnn_in
+        #self.fc1 = nn.Linear(dim, 64)
+        #self.fc2 = nn.Linear(64, 32)
+        #self.fc3 = nn.Linear(32, 16)
+        #self.fc4 = nn.Linear(16, num)
+        self.fc1 = nn.Linear(dim_dnn_in, 128)
+        self.fc2 = nn.Linear(128, 128)
+        #self.fc3 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(128, dim_dnn_out)
+        #self.fc4 = nn.Linear(64, num)
+        #sigmoid=0/1
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc3(x))
+        y_hat=self.sigmoid(self.fc3(x))    
+        #return self.decode(z), x
+        return y_hat
+    
---
+++ sourcemodel.py
@@ -0,0 +1,222 @@
+
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+from scipy.stats import pearsonr
+from sklearn import linear_model, preprocessing
+from sklearn.manifold import TSNE
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, TensorDataset
+
+from models import AE, DNN
+
+# Define parameters
+epochs = 500 #200,500,1000
+dim_au_in = 11833
+dim_au_out = 512 #8, 16, 32, 64, 128, 256,512
+dim_dnn_in = dim_au_out
+dim_dnn_out=1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # NN
+    parser.add_argument('--data_path', type=str, default='data/GDSCexpression.csv')
+    parser.add_argument('--label_path', type=str, default='data/GDSClabel.csv')
+
+    parser.add_argument('--trained', type=str, default='')
+    parser.add_argument('--slope', type=float, default=0.1)
+    # train
+    parser.add_argument('--lr', type=float, default=2e-4)
+    parser.add_argument('--weight_decay', type=float, default=2.5e-5)
+    parser.add_argument('--epochs', type=int, default=512)
+    parser.add_argument('--batch_size', type=int, default=200)
+    parser.add_argument('--bottleneck', type=int, default=512)
+    parser.add_argument('--dimreduce', type=str, default="AE")
+    parser.add_argument('--predictor', type=str, default="DNN")
+
+
+    # misc
+    parser.add_argument('--n_workers', type=int, default=0)
+    parser.add_argument('--logdir', type=str, default='outputs/garbage')
+    parser.add_argument('--message', '-m',  type=str, default='')
+    args, unknown = parser.parse_known_args()
+    main(args)
+
+
+def main(args):
+
+    # Define parameters
+    epochs = args.epochs
+    dim_au_out = args.bottleneck #8, 16, 32, 64, 128, 256,512
+    dim_dnn_in = dim_au_out
+    dim_dnn_out=1
+
+    data_path = args.data_path
+    label_path = args.label_path
+
+    data_r=pd.read_csv(data_path)
+    label_r=pd.read_csv(label_path)
+
+    label_r=label_r.fillna(1)
+    data = data_r
+    label = label_r.iloc[:,10]
+    scaler = preprocessing.StandardScaler(with_mean=True, with_std=True)
+    data = scaler.fit_transform(data)
+
+    print(np.std(data))
+    print(np.mean(data))
+
+    X_train, X_test, Y_train, Y_test = train_test_split(data, label, test_size=0.2, random_state=42)
+
+    print(data.shape)
+    print(label.shape)
+    print(X_train.shape, Y_train.shape)
+    print(X_test.shape, Y_test.shape)
+    print(X_train.max())
+    print(X_train.min())
+
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Assuming that we are on a CUDA machine, this should print a CUDA device:
+    print(device)
+    torch.cuda.set_device(device)
+
+
+    trainData = torch.FloatTensor(X_train).to(device)
+    testData = torch.FloatTensor(X_test).to(device)
+    y = torch.FloatTensor(Y_train.values).to(device)
+    allData = torch.FloatTensor(data).to(device)
+
+    # construct TensorDataset
+    train_dataset = TensorDataset(trainData, trainData)
+    test_dataset = TensorDataset(testData, testData)
+    all_dataset = TensorDataset(allData, allData)
+
+    trainDataLoader1 = DataLoader(dataset=train_dataset, batch_size=200, shuffle=False)
+    trainDataLoaderall = DataLoader(dataset=all_dataset, batch_size=200, shuffle=False)
+
+
+    autoencoder = AE(dim_au_in = X_train.shape[1],dim_au_out=dim_au_out).to(device)
+    optimizer = optim.Adam(autoencoder.parameters(), lr=1e-3)
+    loss_func = nn.SmoothL1Loss().to(device)
+    loss_train = np.zeros((epochs, 1))
+
+
+
+    for epoch in range(epochs):
+        # 
+        for batchidx, (x, _) in enumerate(trainDataLoaderall):
+            x.requires_grad_(True)
+            # encode and decode 
+            decoded, encoded = autoencoder(x)
+            # compute loss
+            print(encoded.shape, decoded.shape)
+            loss = loss_func(decoded, x)      
+            # update
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+        loss_train[epoch,0] = loss.item()  
+        print('Epoch: %04d, Training loss=%.8f' %
+            (epoch+1, loss.item()))
+    
+    torch.save(autoencoder.state_dict(), 'saved/models/'+data_path+args.dimreduce+'.pkl')
+    
+    # extract features
+    _, encodedTrainData = autoencoder(trainData)
+    featureTensor = encodedTrainData.double()
+    feature = featureTensor.detach().cpu().numpy()
+
+
+
+    print(feature.shape)
+
+
+    clf = linear_model.Lasso(alpha=0.1)
+    clf.fit(feature, Y_train.values)
+
+
+    # In[23]:
+
+
+    _,testFeature = autoencoder(testData)
+    lasso = clf.predict(testFeature.detach().cpu().numpy())
+
+
+    print(r2_score(lasso,Y_test))
+
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(feature)
+    plt.scatter(tsne_results[:, 0], tsne_results[:, 1], label="label")
+    plt.legend()
+    plt.savefig("saved/figrues/"+data_path+args.dimreduce+args.predictor+".png")
+
+
+    EPOCH = 500
+
+
+    # In[30]:
+
+
+    # Load data
+    # data type conversion
+    B_feature = torch.FloatTensor(feature).to(device)
+    y = torch.FloatTensor(Y_train.values).to(device)
+    # construct TensorDataset
+    b_data = TensorDataset(B_feature, y)
+    trainDataLoader2 = DataLoader(dataset=b_data, batch_size=200, shuffle=False)
+
+
+    # In[31]:
+
+
+
+    predictor = DNN(dim_dnn_in, dim_dnn_out).to(device)
+    optimizer = optim.Adam(predictor.parameters(), lr=1e-3,betas=(0.9,0.99))
+    loss_func = nn.BCELoss().to(device)
+    loss_train = np.zeros((epochs, 1))
+
+    # train model
+    for epoch in range(EPOCH):
+        print('Epoch: ',epoch)
+        for step,(batch_x,batch_y) in enumerate(trainDataLoader2):
+            b_x = Variable(batch_x)
+            b_y = Variable(batch_y)
+            # predict label
+            output = predictor(b_x)
+            # b_y=F.sigmoid(b_y) 
+            
+            #print
+            #print(output)
+            #print(b_y)
+            # compute loss
+            loss = loss_func(output,b_y)
+            #loss = criterion(output, b_y)
+            
+            # update
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        loss_train[epoch,0] = loss.item()  
+        print('Epoch: %04d, Training loss=%.8f' %
+            (epoch+1, loss.item())) 
+
+    torch.save(predictor.state_dict(), 'saved/models/'+data_path+args.predictor+'.pkl')
+
+
+    # Get tesing feature
+    _,testFeature = autoencoder(testData)
+    testpredict = predictor(testFeature)
+    r2_score(testpredict.detach().cpu().numpy(),Y_test)
+    mean_squared_error(testpredict.detach().cpu().numpy(),Y_test)
