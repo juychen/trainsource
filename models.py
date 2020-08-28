d13cdca4d2bd41a02d062ab7841f4a4df389e9b6
WINGHARE <WINGHARE@GMAIL.COM>
2020-08-28 15:10:15 +0800

    Edit

--- models.py
+++ models.py
@@ -62,3 +62,53 @@ class DNN(nn.Module):
         #return self.decode(z), x
         return y_hat
     
+
+class VAE(nn.Module):
+    def __init__(self,dim_au_in,dim_au_out=512):
+        super(VAE, self).__init__()
+
+        self.input_shape = dim_au_in
+        self.bottleneck_shape = dim_au_out
+
+
+        self.fc1 = nn.Linear(dim_au_in, 1024)
+        self.fc21 = nn.Linear(1024, dim_au_out)
+        self.fc22 = nn.Linear(1024, dim_au_out)
+        self.fc3 = nn.Linear(dim_au_out, 1024)
+        self.fc4 = nn.Linear(1024, dim_au_in)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        if torch.cuda.is_available():
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return F.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def loss_function(recon_x, x, mu, logvar):
+    """
+    recon_x: generating images
+    x: origin images
+    mu: latent mean
+    logvar: latent log variance
+    """
+        BCE = reconstruction_function(recon_x, x)  # mse loss
+        # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+        KLD = torch.sum(KLD_element).mul_(-0.5)
+        # KL divergence
+        return BCE + KLD
