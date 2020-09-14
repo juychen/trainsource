7a45b35a0334d7bae6fe0ed2d7d7d8d97d932dad
Junyi CHEN <winghare@gmail.com>
2020-09-14 10:44:48 +0800

    Edit the util for hvgs

---
+++ utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+
+def highly_variable_genes(data, 
+    layer=None, n_top_genes=None, 
+    min_disp=0.5, max_disp=np.inf, min_mean=0.0125, max_mean=3, 
+    span=0.3, n_bins=20, flavor='seurat', subset=False, inplace=True, batch_key=None):
+
+    adata = sc.AnnData(data)
+
+    adata.var_names_make_unique()  # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
+    adata.obs_names_make_unique()
+
+
+    if n_top_genes!=None:
+        sc.pp.highly_variable_genes(adata,layer=layer,n_top_genes=n_top_genes,
+        span=span, n_bins=n_bins, flavor='seurat_v3', subset=subset, inplace=inplace, batch_key=batch_key)
+
+    else: 
+        sc.pp.log1p(adata)
+        sc.pp.highly_variable_genes(adata,
+        layer=layer,n_top_genes=n_top_genes,
+        min_disp=min_disp, max_disp=max_disp, min_mean=min_mean, max_mean=max_mean, 
+        span=span, n_bins=n_bins, flavor=flavor, subset=subset, inplace=inplace, batch_key=batch_key)
+
+    return adata.var.highly_variable,adata
