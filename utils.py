e4af44e6175a3187c94430b2faada4480cdcdf84
Junyi CHEN <winghare@gmail.com>
2020-09-18 16:03:36 +0800

    Edit the trainning models

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
+import copy
+
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import torch
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, TensorDataset
+
 
 def highly_variable_genes(data, 
     layer=None, n_top_genes=None, 
@@ -25,3 +33,143 @@ def highly_variable_genes(data,
         span=span, n_bins=n_bins, flavor=flavor, subset=subset, inplace=inplace, batch_key=batch_key)
 
     return adata.var.highly_variable,adata
+
+def train_extractor_model(net,data_loaders={},optimizer=None,loss_function=None,n_epochs=100,scheduler=None,load=None,save_path="model.pkl"):
+    
+    if(load!=None):
+        net.load_state_dict(torch.load(load))           
+    
+        return net, 0
+    
+    dataset_sizes = {x: data_loaders[x].dataset.tensors[0].shape[0] for x in ['train', 'val']}
+    loss_train = {}
+    
+    best_model_wts = copy.deepcopy(net.state_dict())
+    best_loss = np.inf
+
+    for epoch in range(n_epochs):
+        print('Epoch {}/{}'.format(epoch, n_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                #optimizer = scheduler(optimizer, epoch)
+                net.train()  # Set model to training mode
+            else:
+                net.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+
+            # Iterate over data.
+            # for data in data_loaders[phase]:
+            for batchidx, (x, _) in enumerate(data_loaders[phase]):
+
+                x.requires_grad_(True)
+                # encode and decode 
+                output = net(x)
+                # compute loss
+                loss = loss_function(output, x)      
+
+                # zero the parameter (weight) gradients
+                optimizer.zero_grad()
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    # update the weights
+                    optimizer.step()
+
+                # print loss statistics
+                running_loss += loss.item()
+            
+            # Schedular
+#             if phase == 'train':
+#                 scheduler.step()
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+            
+            if phase == 'train':
+                scheduler.step(epoch_loss)
+                
+            last_lr = scheduler.optimizer.param_groups[0]['lr']
+            loss_train[epoch,phase] = epoch_loss
+            print('{} Loss: {:.8f}. Learning rate = {}'.format(phase, epoch_loss,last_lr))
+            
+            if phase == 'val' and epoch_loss < best_loss:
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(net.state_dict())
+    
+    # Select best model wts
+    torch.save(best_model_wts, save_path)
+    net.load_state_dict(best_model_wts)           
+    
+    return net, loss_train
+
+def train_predictor_model(net,data_loaders,optimizer,loss_function,n_epochs,scheduler,save_path="model.pkl"):
+    
+    dataset_sizes = {x: data_loaders[x].dataset.tensors[0].shape[0] for x in ['train', 'val']}
+    loss_train = {}
+    
+    best_model_wts = copy.deepcopy(net.state_dict())
+    best_loss = np.inf
+
+    for epoch in range(n_epochs):
+        print('Epoch {}/{}'.format(epoch, n_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                #optimizer = scheduler(optimizer, epoch)
+                net.train()  # Set model to training mode
+            else:
+                net.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+
+            # Iterate over data.
+            # for data in data_loaders[phase]:
+            for batchidx, (x, y) in enumerate(data_loaders[phase]):
+
+                x.requires_grad_(True)
+                # encode and decode 
+                output = net(x)
+                # compute loss
+                loss = loss_function(output, y)      
+
+                # zero the parameter (weight) gradients
+                optimizer.zero_grad()
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    # update the weights
+                    optimizer.step()
+
+                # print loss statistics
+                running_loss += loss.item()
+            
+            # Schedular
+#             if phase == 'train':
+#                 scheduler.step()
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+            
+            if phase == 'train':
+                scheduler.step(epoch_loss)
+                
+            last_lr = scheduler.optimizer.param_groups[0]['lr']
+            loss_train[epoch,phase] = epoch_loss
+            print('{} Loss: {:.8f}. Learning rate = {}'.format(phase, epoch_loss,last_lr))
+            
+            if phase == 'val' and epoch_loss < best_loss:
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(net.state_dict())
+    
+    # Select best model wts
+        torch.save(best_model_wts, save_path)
+        
+    net.load_state_dict(best_model_wts)           
+    
+    return net, loss_train
