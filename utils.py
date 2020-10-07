7c0e855d052af91404a135a68647a9ddf7baea9a
Junyi CHEN <winghare@gmail.com>
2020-10-07 20:58:58 +0800

    Edit the logic of adv trainnig

--- utils.py
+++ utils.py
@@ -4,11 +4,12 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 import torch
-from torch import nn, optim
+from torch import nn, optim, t
 from torch.autograd import Variable
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, TensorDataset
 import matplotlib.pyplot as plt
+from torch.utils.data import dataset
 
 
 
@@ -176,10 +177,132 @@ def train_predictor_model(net,data_loaders,optimizer,loss_function,n_epochs,sche
     
     return net, loss_train
 
+def train_transfer_model(
+    source_encoder, target_encoder, discriminator,
+    source_loader, target_loader,
+    dis_loss, target_loss,
+    optimizer, d_optimizer,
+    scheduler,d_scheduler,
+    n_epochs,save_path="model.pkl",
+    args=None):
+    
+    target_dataset_sizes = {x: target_loader[x].dataset.tensors[0].shape[0] for x in ['train', 'val']}
+    source_dataset_sizes = {x: source_loader[x].dataset.tensors[0].shape[0] for x in ['train', 'val']}
+
+    dataset_sizes = {x: min(target_dataset_sizes[x],source_dataset_sizes[x]) for x in ['train', 'val']}
+
+    loss_train = {}
+    
+    # best_target_wts = copy.deepcopy(target_encoder.state_dict())
+    # best_discm_wts = copy.deepcopy(discriminator.state_dict())
+
+    # best_loss = np.inf
+
+    for epoch in range(n_epochs):
+        print('Epoch {}/{}'.format(epoch, n_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                #optimizer = scheduler(optimizer, epoch)
+                source_encoder.eval()
+                target_encoder.train()  # Set model to training mode
+                discriminator.train()  # Set model to training mode
+
+            else:
+                source_encoder.eval()
+                target_encoder.eval()  # Set model to evaluate mode
+                discriminator.eval()  # Set model to training mode
+
+            running_loss = 0.0
+            d_running_loss = 0.0
+
+                #losses, d_losses = AverageMeter(), AverageMeter()
+            n_iters = min(len(source_loader[phase]), len(target_loader[phase]))
+            source_iter, target_iter = iter(source_loader[phase]), iter(target_loader[phase])
+
+            # Iterate over data.
+            # for data in data_loaders[phase]:
+            for iter_i in range(n_iters):
+                source_data, source_target = source_iter.next()
+                target_data, target_target = target_iter.next()
+                source_data = source_data.to(args.device)
+                target_data = target_data.to(args.device)
+                bs = source_data.size(0)
+
+                D_input_source = source_encoder.encoder(source_data)
+                D_input_target = target_encoder.encoder(target_data)
+                D_target_source = torch.tensor(
+                    [0] * bs, dtype=torch.long).to(args.device)
+                D_target_target = torch.tensor(
+                    [1] * bs, dtype=torch.long).to(args.device)
+
+                # train Discriminator
+                D_output_source = discriminator(D_input_source)
+                D_output_target = discriminator(D_input_target)
+                D_output = torch.cat([D_output_source, D_output_target], dim=0)
+                D_target = torch.cat([D_target_source, D_target_target], dim=0)
+                d_loss = dis_loss(D_output, D_target)
+
+
+                d_optimizer.zero_grad()
+
+                if phase == 'train':
+                    d_loss.backward()
+                    d_optimizer.step()
+                
+                d_running_loss += d_loss.item()
+
+                D_input_target = target_encoder.encoder(target_data)
+                D_output_target = discriminator(D_input_target)
+                loss = target_loss(D_output_target, D_target_source)
+
+                optimizer.zero_grad()
+
+
+                if phase == 'train':
+
+                    loss.backward()
+                    optimizer.step()
+                
+                running_loss += loss.item()
+            
+            # Schedular
+#             if phase == 'train':
+#                 scheduler.step()
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+            d_epoch_loss = d_running_loss / dataset_sizes[phase]
+
+            if phase == 'train':
+                scheduler.step(epoch_loss)
+                d_scheduler.step(d_epoch_loss)
+                
+            last_lr = scheduler.optimizer.param_groups[0]['lr']
+            d_last_lr = d_scheduler.optimizer.param_groups[0]['lr']
+
+            loss_train[epoch,phase] = epoch_loss
+            print('Discriminator {} Loss: {:.8f}. Learning rate = {}'.format(phase, d_epoch_loss,d_last_lr))
+            print('Encoder {} Loss: {:.8f}. Learning rate = {}'.format(phase, epoch_loss,last_lr))
+
+            # if phase == 'val' and epoch_loss < best_loss:
+            #     best_loss = epoch_loss
+            #     best_model_wts = copy.deepcopy(net.state_dict())
+    
+    # Select best model wts
+        torch.save(discriminator.state_dict(), save_path+"_d.pkl")
+        torch.save(target_encoder.state_dict(), save_path+"_te.pkl")
+
+    #net.load_state_dict(best_model_wts)           
+    
+    return discriminator,target_encoder, loss_train
+
+
 def train_adversarial_model(
     source_encoder, target_encoder, discriminator,
     source_loader, target_loader,
-    souce_loss, target_loss,
+    dis_loss, target_loss,
     optimizer, d_optimizer,
     args=None
 ):
@@ -209,20 +332,20 @@ def train_adversarial_model(
         D_output_target = discriminator(D_input_target)
         D_output = torch.cat([D_output_source, D_output_target], dim=0)
         D_target = torch.cat([D_target_source, D_target_target], dim=0)
-        d_loss = criterion(D_output, D_target)
+        d_loss = dis_loss(D_output, D_target)
         d_optimizer.zero_grad()
         d_loss.backward()
         d_optimizer.step()
-        d_losses.update(d_loss.item(), bs)
+        #d_losses.update(d_loss.item(), bs)
 
         # train Target
         D_input_target = target_encoder.encoder(target_data)
         D_output_target = discriminator(D_input_target)
-        loss = criterion(D_output_target, D_target_source)
+        loss = target_loss(D_output_target, D_target_source)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        losses.update(loss.item(), bs)
+        #losses.update(loss.item(), bs)
     return {'d/loss': d_losses.avg, 'target/loss': losses.avg}
 
 def plot_label_hist(Y,save=None):
