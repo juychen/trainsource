6d9b5afdf58c36c070e2ee04e66cf1264a2fffce
Junyi CHEN <winghare@gmail.com>
2020-12-02 16:39:56 +0800

    Edit source model

--- sourcemodel.py
+++ sourcemodel.py
@@ -94,7 +94,7 @@ def run_main(args):
     #log=open(log_path,"w")
     #sys.stdout=log
 
-    logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
+    logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
                     filename=log_path,
                     filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
                     #a是追加模式，默认如果不写的话，就是追加模式
