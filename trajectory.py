a0d1cda4f591ee992c8087d46ac49c127465787f
WINGHARE <winghare@hotmail.com>
2021-01-23 20:06:49 +0800

    Edit The bug in traj

--- trajectory.py
+++ trajectory.py
@@ -6,29 +6,29 @@ import scanpy as sc
 
 
 def trajectory(adata):
-    sc.settings.verbosity = 3  # verbosity: errors (0), warnings (1), info (2), hints (3)
+    sc.settings.verbosity = 0  # verbosity: errors (0), warnings (1), info (2), hints (3)
     #sc.logging.print_versions()
-    sc.settings.set_figure_params(dpi=80, frameon=False, figsize=(3, 3), facecolor='white')  # low dpi (dots per inch) yields small inline figures
+    #sc.settings.set_figure_params(dpi=80, frameon=False, figsize=(3, 3), facecolor='white')  # low dpi (dots per inch) yields small inline figures
     #adata2=sc.read_h5ad("./GSE112274_Gef2020-12-27-17-56-31.h5ad")
     adata2=adata
     adata2.X=adata2.X.astype('float64')
     
     ##draw
     sc.tl.draw_graph(adata2)
-    sc.pl.draw_graph(adata2, color='sens_preds')
+    #sc.pl.draw_graph(adata2, color='sens_preds')
     sc.pl.draw_graph(adata2, color='leiden')
-    sc.pl.draw_graph(adata2, color='leiden_Pret')
+    sc.pl.draw_graph(adata2, color='leiden_Trans')
     
     
     
     ## trajectory1
-    sc.tl.paga(adata2, groups='leiden_Pret')
-    sc.pl.paga(adata2, color=['leiden_Pret'])
-    adata2.obs['leiden_Pret'].cat.categories
-    sc.tl.paga(adata2, groups='leiden_Pret')
+    sc.tl.paga(adata2, groups='leiden_trans')
+    sc.pl.paga(adata2, color='leiden_trans')
+    adata2.obs['leiden_trans'].cat.categories
+    sc.tl.paga(adata2, groups='leiden_trans')
     sc.pl.paga(adata2, threshold=0.03, show=False)
     sc.tl.draw_graph(adata2, init_pos='paga')
-    sc.pl.draw_graph(adata2, color=['leiden_Pret'], legend_loc='on data')
+    sc.pl.draw_graph(adata2, color=['leiden_trans'], legend_loc='on data')
     pl.figure(figsize=(8, 2))
     for i in range(8):
         pl.scatter(i, 1, c=sc.pl.palettes.zeileis_28[i], s=200)
@@ -38,7 +38,7 @@ def trajectory(adata):
         adata2, threshold=0.03, title='', right_margin=0.2, size=10,
         edge_width_scale=0.5,legend_fontsize=12, fontsize=12, frameon=False,
         edges=True, save=True)
-    adata2.uns['iroot'] = np.flatnonzero(adata2.obs['leiden_Pret']  == '0')[0]
+    adata2.uns['iroot'] = np.flatnonzero(adata2.obs['leiden_trans']  == '0')[0]
     sc.tl.dpt(adata2)
     adata_raw = adata2
     sc.pp.log1p(adata_raw)
@@ -51,7 +51,7 @@ def trajectory(adata):
     
     ## trajectory2
     sc.tl.paga(adata2, groups='leiden')
-    sc.pl.paga(adata2, color=['leiden'])
+    sc.pl.paga(adata2, color='leiden')
     adata2.obs['leiden'].cat.categories
     sc.tl.paga(adata2, groups='leiden')
     sc.pl.paga(adata2, threshold=0.03, show=False)
