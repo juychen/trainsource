f04574579df0b1032f2cd95dcb06fbe630fa077a
WINGHARE <winghare@hotmail.com>
2021-01-26 10:20:19 +0800

    Edit the annoatuons

--- transfer.py
+++ transfer.py
@@ -87,13 +87,11 @@ def run_main(args):
     sys.stderr=out
     
     #Logging infomaion
-    logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
+    logging.basicConfig(level=logging.INFO,
                     filename=log_path,
-                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
-                    #a是追加模式，默认如果不写的话，就是追加模式
+                    filemode='a',
                     format=
                     '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
-                    #日志格式
                     )
     logging.getLogger('matplotlib.font_manager').disabled = True
 
