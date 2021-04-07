4601fb37463942cf556cf752a2454b57f5c0f3be
WINGHARE <winghare@hotmail.com>
2021-04-07 23:14:11 +0800

    Edit the integrated gradients

--- transfer.py
+++ transfer.py
@@ -471,20 +471,19 @@ def run_main(args):
             target_model = TargetModel(source_model,encoder)
 
             # Allow require gradients and process label
-            Xtarget_validTensor.requires_grad_()
             Xtarget_trainTensor.requires_grad_()
 
             # Run integrated gradient check
             # Return adata and feature integrated gradient
 
-            ytarget_validPred = target_model(Xtarget_trainTensor).detach().cpu().numpy()
-            ytarget_validPred = ytarget_validPred.argmax(axis=1)
+            ytarget_trainPred = target_model(Xtarget_trainTensor).detach().cpu().numpy()
+            ytarget_trainPred = ytarget_trainPred.argmax(axis=1)
 
-            adata,attr1,df_top1_genes,df_tail1_genes = ut.integrated_gradient_check(net=target_model,input=Xtarget_trainTensor,target=ytarget_validPred
+            adata,attr1,df_top1_genes,df_tail1_genes = ut.integrated_gradient_check(net=target_model,input=Xtarget_trainTensor,target=ytarget_trainPred
                                         ,adata=adata,n_genes=args.n_DL_genes
                                         ,save_name=reduce_model + args.predictor+ prediction + select_drug+now)
 
-            adata,attr0,df_top0_genes,df_tail0_genes = ut.integrated_gradient_check(net=target_model,input=Xtarget_trainTensor,target=ytarget_validPred,
+            adata,attr0,df_top0_genes,df_tail0_genes = ut.integrated_gradient_check(net=target_model,input=Xtarget_trainTensor,target=ytarget_trainPred,
                                         target_class=0,adata=adata,n_genes=args.n_DL_genes
                                         ,save_name=reduce_model + args.predictor+ prediction + select_drug+now)
 
